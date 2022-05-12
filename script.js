dfbe8aa4f25623f15ae39f2f0c8a0f306f25c98f
alihpss <ali.hps@hotmail.com>
2022-05-12 17:18:28 -0300

    deixando código mais limpo

--- script.js
+++ script.js
@@ -6,27 +6,22 @@ function tarefaM (string) {
 
 function criarItem(){
 
-//          criando label para poder acessar input e div 
     const novoItem = document.createElement('label')
     novoItem.classList.add('tarefas')
     novoItem.setAttribute('for', String(tarefaAtual.value))
 
-//          criando input checkbox e alterando seus atributos
     const check = document.createElement('input')
     check.classList.add('tarefas')
     check.setAttribute('type', 'checkbox')
     check.setAttribute('name', String(tarefaAtual.value))
 
-//          selecionando lista de tarefas e atualizando a mesma
     let listaTarefas = document.getElementById('res').appendChild(novoItem)
     listaTarefas.appendChild(check);
 
-//          criando div do texto
     const bloco = document.createElement('div')
     bloco.textContent = tarefaM(tarefaAtual.value);
     novoItem.appendChild(bloco)
 
-//          criando botão para remover itens
     const remove = document.createElement('input')
     remove.classList.add('tarefas')
     remove.setAttribute('type', 'button')
@@ -34,8 +29,10 @@ function criarItem(){
     bloco.appendChild(remove)
 
 
-    // Função para remover os itens
-    remove.onclick = function() {apagar()};
+ 
+    remove.onclick = function() {
+        apagar()
+    };
 
 
     function apagar() {
